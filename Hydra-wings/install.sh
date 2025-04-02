51291793f5e353a41d831d7437a4be19010b18d3
itsnothacker <dj@itsnothacker.run.place>
2025-04-02 17:26:04 +0530

    Update install.sh

--- Hydra-wings/install.sh
+++ Hydra-wings/install.sh
@@ -2,6 +2,7 @@ echo "Installing Hydra Wings..."
 git clone https://github.com/hydren-dev/HydraDAEMON
 cd HydraDAEMON || exit
 sudo apt install nodejs
+sudo apt install npm
 npm i
 clear
 echo Go To Panel and Create a Node then past config file here. then "node ." to run it.
